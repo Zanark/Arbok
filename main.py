e9d232582c2f7b054c1eecbd8ae662fb1e745636
Zanark <kiodeba11@gmail.com>
2018-07-13 23:06:58 +0530

    Basic settings

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+#print (x)
+
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('ARBOK')
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+        #print(event)
+pygame.quit()
