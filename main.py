84a6caf53a9f9a2c5d96982ead39095a970868fc
Zanark <kiodeba11@gmail.com>
2018-07-16 20:49:47 +0530

    No more Diagonal movement

--- main.py
+++ main.py
@@ -25,19 +25,22 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x_change = -5
-            if event.key == pygame.K_RIGHT:
+                lead_y_change = 0
+            elif event.key == pygame.K_RIGHT:
                 lead_x_change = 5
-            if event.key == pygame.K_UP:
+                lead_y_change = 0
+            elif event.key == pygame.K_UP:
                 lead_y_change = -5
-            if event.key == pygame.K_DOWN:
+                lead_x_change = 0
+            elif event.key == pygame.K_DOWN:
                 lead_y_change = 5
-        if event.type == pygame.KEYUP:
+                lead_x_change = 0
+        '''if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 lead_x_change = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                lead_y_change = 0
-        
-        #print(event)
+                lead_y_change = 0'''
+
     lead_x += lead_x_change
     lead_y += lead_y_change
 
@@ -45,6 +48,6 @@ while not gameExit:
     pygame.draw.rect(gameDisplay , black , [lead_x , lead_y , 10 , 10])
     pygame.display.update()
 
-    clock.tick(10)
+    clock.tick(15)
 
 pygame.quit()
